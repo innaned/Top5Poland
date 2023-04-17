72f4a26643bd6c6e9a7ce1a73fbd6266a8c6d0b7
innaned <nedelko2007@gmail.com>
2023-04-17 22:01:51 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
+
+  const [places, setPlaces] = useState(data);
+
+  const [showText, setShowText] = useState(false);
+
+  const removePlace=(id) => {
+    let newPlaces = places.filter (place=>place.id !==id);
+    setPlaces(newPlaces)
+  }
+
+  const showTextClick =(place) => {
+    place.showMore = !place.showMore
+    setShowText(!showText)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+        <h1>TOP {places.length} najpiękniejszych miejsc do zwiedzania w Polsce</h1>
+      </div>
+
+      {places.map((place => {
+        const {id, title, description, image, showMore} = place;
+
+        return(
+          <div key ={id}>
+            <div className='container'>
+              <h2>{title}</h2>
+            </div>
+
+            <div className='container'>
+              <p>{showMore ? description : description.substring(0, 190) + "..."}
+              <button onClick={() => showTextClick(place)}>{showMore ? "mniej" : "więcej"}</button>
+              </p>
+            </div>
+
+            <div className='container'>
+              <img src={image} width="700px" alt='Zamek w Mosznej'/>
+            </div>
+
+            <div className='container'>
+              <button onClick={() => removePlace(id)} className='btn'>Usunąć z listy</button>
+            </div>
+          </div>
+        )
+      }))}
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,55 @@
+import imageOne from './moszna.jpg';
+import imageTwo from './morskieOko.jpg';
+import imageThree from './wroclaw.jpg';
+import imageFour from './Ksiaz.jpg';
+import imageFive from './bieszczady.jpg'
+
+export const data = [
+    {
+        id: 1,
+        title:"Zamek Moszna",
+        description: "Pałac w Mosznej (niem. Schloss Moschen) - zabytkowa rezydencja położona we wsi Moszna, w województwie opolskim, pomiędzy miastami Prudnik i Krapkowice. Pałac posiada 365 pomieszczeń i 99 wież i wieżyczek. Obiekt otacza ponad stuhektarowy park, który jest częścią parku krajobrazowego z cennym drzewostanem, trzystuletnie okazy dębów i rzadkimi skupiskami rododendronów. Jedynie oś główna całego konceptu parku ma geometryczny charakter. Tuż za fontanną znajduje się aleja lipowa na końcu której znajduje marmurowy cokół - pozostałość po pomniku Huberta von Tiele - Wincklera. Po obu stronach alei zachowały się XIX-wieczne kanały wodne, zbudowane w stylu holenderskim i francuskim oraz malownicze stawy.",
+        image: imageOne,
+        showMore: false
+    },
+
+    {
+        id: 2,
+        title:"Morskie Oko w Tatrach",
+        description: "Z Morskim Okiem wiąże się wiele góralskich podań i legend. Według jednej z nich nazwa „bezdennego” jeziora w Dolinie Rybiego Potoku wzięła się stąd, że ma ono jakoby podziemne połączenie z Adriatykiem. Dowodem na to było rzekomo wyłowienie z jego wód butelki i szkatułki z kosztownościami, które razem ze statkiem poszły na dno morza. Urok Morskiego Oka inspirował twórczość wielu artystów, zarówno malarzy, jak i poetów czy muzyków. Morskie Oko o powierzchni 34,93 ha jest największym jeziorem w Tatrach. Morskie Oko jest jedynym z nielicznych naturalnie zarybionych jezior na terenie Tatr. Znajduje się w nim stanowisko pstrągów i lipieni",
+        image: imageTwo,
+        showMore: false
+    },
+
+    {
+        id: 3,
+        title:"Wrocław",
+        description: "Wrocław jest jednym z najstarszych miast Polski pod względem lokacji na prawach miejskich. We Wrocławiu mimo zniszczeń wojennych zachowało się wiele budowli w stanie oryginalnym, bądź odrestaurowanych czy odbudowanych po wojnie. Do wyróżniających się należą: dwa ratusze na Rynku - gotycki Stary Ratusz oraz Nowy Ratusz w stylu historyzmu; gotyckie kościoły. Oprócz tego w mieście zachowało się ponad osiem tysięcy zabytkowych kamienic reprezentujących style historyczne pochodzących z XIX i początków XX wieku. Wiele kamienic prezentuje wysokie walory architektoniczno-estetyczne. W niektórych przypadkach są to zachowane całe ciągi ulic architektury historyzującej jak chociażby ulica Miernicza oraz plac Księdza Stanisława Staszica na osiedlu Nadodrze. Na wrocławskim Tarnogaju zachowało się osiedle mieszkaniowe domków jednorodzinnych przy ul. Kamienieckiej, która bywa nazywana wrocławską „Złotą Uliczką. W budynku Banku Zachodniego na rogu Rynku i placu Solnego działa jedyny we Wrocławiu paternoster, czyli winda bez drzwi, która jeździ ze stałą prędkością, nie zatrzymując się na poszczególnych piętrach. Jako jedno z nielicznych miast w Europie ma zachowaną i stale wypełnioną wodą fosę miejską. Wrocławski ratusz jest jednym z najstarszych ratuszy w Polsce. Na wysokiej wieży mieści się najstarszy w Polsce dzwon zegarowy i najstarszy, z 1368, zegar wieżowy. W piwnicach budynku znajduje się najstarsza (z XIV wieku) restauracja w Europie - Piwnica Świdnicka.",
+        image: imageThree,
+        showMore: false
+    },
+
+    {
+        id: 4,
+        title:"Zamek Książ",
+        description: "Książ (niem. Fürstenstein) - kompleks zamkowy, zlokalizowany w wałbrzyskiej dzielnicy Książ na Pogórzu Wałbrzyskim, będący częścią Książańskiego Parku Krajobrazowego. Przez obiekt przebiega Szlak Zamków Piastowskich i jest trzecim co do wielkości zamkiem w Polsce. Według legend odnoszących się do dziejów piastowskich na zamku Książ, pierwszą warownię miał w tym miejscu wznieść w połowie X wieku rycerz o imieniu Funkenstein. Jeszcze jako młody chłopak w 933 roku miał on podarować księciu saksońskiemu, Henrykowi I Ptasznikowi, worek wykopanego w lesie węgla, sytuacja miała ucieszyć władcę do tego stopnia, że w nagrodę nadał mu godność szlachecką wraz z przydomkiem Tego Który Przynosi Kamienie Dające Iskry. Nakazał mu również powrócić na Śląsk, odnaleźć dogodne miejsce i zbudować w jego sąsiedztwie obronny gród, aby z jego pomocą strzec odtąd czarnego skarbu.16 października 2018 r., została otwarta „Podziemna Trasa Turystyczna” w tunelach drążonych podczas II wojny światowej[28]. W tym samym roku zamek znalazł się na liście „7 cudów Polski” na 100-lecie Niepodległości Polski.",
+        image: imageFour,
+        showMore: false
+    },
+
+    {
+        id: 5,
+        title:"Magiczne Bieszczady",
+        description: "Bieszczady należą do Beskidów Wschodnich, które są częścią Zewnętrznych Karpat Wschodnich. Bieszczady składają się z dwóch pasm górskich ulokowanych w łańcuchu Karpat. Dzielą się na Bieszczady Zachodnie, które są zarówno na polskiej stronie, jak i ukraińskiej oraz na Bieszczady Wschodnie, które ulokowały się jedynie po stronie ukraińskiej. Jako ciekawostkę dodamy, że pomimo tego, że w ich górnych partiach znajdziecie połoniny, to Bieszczady kwalifikują się do Beskidów Lesistych, a nie do Beskidów Połonińskich, które znajdziecie na Ukrainie oraz Rumunii. W Bieszczadach występują rośliny i zwierzęta niespotykane w innych częściach Polski.",
+        image: imageFive,
+        showMore: false
+    },
+
+
+
+
+
+
+
+
+]
